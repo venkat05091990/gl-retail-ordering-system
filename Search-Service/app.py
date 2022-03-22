2a4746af94667297849b24e9336272a71baa1f08
JDA\j1010786 <bharat.surana@blueyonder.com>
2022-03-22 12:27:26 +0530

    Search Service Initial Commit

---
+++ Search-Service/app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 16:42:03 2022
+
+@author: Deeplearn
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.read_csv(r"C:\Users\sudheerkona\Downloads\IIIT_Project\flipkart_data.csv")       
+s=st.text_input("enter text")
+def pred(n,s):
+    temp = [x for x in df.name if s in x.lower()]
+    #count=0
+    #if len(temp)>n:temp=[]
+    if temp==[] or len(temp)>n:
+       # count=1
+        list1 = [x for x in range(len(df.name)) if len(set(s.split()).intersection(set(df.name[x].lower().split())))>0]
+        list2 = [len(set(s.split()).intersection(set(df.name[x].lower().split()))) for x in range(len(df.name)) if len(set(s.split()).intersection(set(df.name[x].lower().split())))>0]
+        df1 = pd.DataFrame([list1,list2]).T
+        df1.columns = ['train_index','match_count']
+        subset = df1.sort_values(by=['match_count'],ascending=False).reset_index(drop=True).iloc[0:n]
+    
+        for k in subset.index:
+            temp.append(df.name[k])
+    return temp
+#    else:
+ #       count =2
+  #      for t in temp:
+   #         st.write(t)
+
+#if st.button("Submit"):
+temp = pred(10,s)
+for c in temp:
+    st.write(c)
+ #   st.write(count)
--- data_preprocessing/get_jobs.py
+++
@@ -1,5 +0,0 @@
-import data_preprocessing_db_update as db
-
-
-if __name__=='__main__':
-    db.get_jobs()
