dbb468fbbdc33a80b30332a2dc7f65a98b806ffa
root <root@ip-172-31-33-254.ec2.internal>
2022-03-26 09:27:55 +0000

    modified scripts

--- bin/deploy-fe-be.sh
+++ bin/deploy-fe-be.sh
@@ -1,3 +1,3 @@
-sudo docker build -t gl-ros-app ../gl-retail-ordering-system/fe-be
+sudo docker build -t gl-ros-app ../fe-be/.
 sudo docker run -it -d -p 5000:5000 -p 3306:3306 gl-ros-app
 sudo docker ps -a
--- bin/deploy-recommendation.sh
+++ bin/deploy-recommendation.sh
@@ -1,3 +1,3 @@
-sudo docker build -t gl-ros-app-recommendation ../gl-retail-ordering-system/Recommendation_services
+sudo docker build -t gl-ros-app-recommendation ../Recommendation_services/.
 sudo docker run -it -d -p 8000:8000 gl-ros-app-recommendation
 sudo docker ps -a
